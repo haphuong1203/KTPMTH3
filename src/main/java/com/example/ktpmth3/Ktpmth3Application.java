960f757d103dfa446a5eb70e51c9c917f20bb7e8
haphuong1203 <haphuongminyoongi@gmail.com>
2022-09-24 16:10:16 +0700

    first commit

---
+++ src/main/java/com/example/ktpmth3/Ktpmth3Application.java
@@ -0,0 +1,13 @@
+package com.example.ktpmth3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ktpmth3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ktpmth3Application.class, args);
+    }
+
+}
